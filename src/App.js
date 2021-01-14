0b4159d3f804e0f67f784718102308bb1dd3facd
sniop.safe@gmail.com <4bmnRq9p>
2021-01-14 21:48:58 +0000

    updates till prop-types

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
+
 
 class App extends Component {
+
+  state = {
+    contacts: [
+      {
+        "id": "karen",
+        "name": "Karen Isgrig",
+        "handle": "karen_isgrig",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+     ]
+  }
+
+  removeContact = (contact) => {
+
+    this.setState((currentState)=>(
+        {
+          contacts:this.state.contacts.filter(c=>c.id!==contact.id)
+        }
+      )
+      
+    ) 
+
+  }
+
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts={this.state.contacts} removeContact={this.removeContact}/>
       </div>
     );
   }
---
+++ src/ListContacts.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// Stateless Function Component
+function ListContacts(props){
+        return (
+            <ol className='contact-list'>
+                {props.contacts.map((contact)=>(
+                    <li key={contact.id} className='contact-list-item'>
+
+                    <div className='contact-avatar' style={
+                        {
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }
+                    }>
+                    </div>
+                    <div className='contact-details'>
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>
+                    </div>
+                    <button className='contact-remove' onClick={()=>props.removeContact(contact)}>Remove</button>
+                    </li>
+                ))}
+            </ol>
+        );
+}
+
+ListContacts.propTypes = {
+    contacts : PropTypes.array.isRequired,
+    removeContact: PropTypes.func.isRequired
+}
+
+export default ListContacts;
