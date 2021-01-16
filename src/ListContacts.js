f8ab366cc00c430b10a3c8b17b4b8ab9e1234370
sniop.safe@gmail.com <4bmnRq9p>
2021-01-16 00:36:23 +0000

    added search and filter

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -20,6 +20,16 @@ class ListContacts extends Component{
     }
 
     render(){
+
+        const {query} = this.state
+        const {contacts, removeContact} = this.props
+
+        const shownContacts = query==''
+                ?contacts
+                :contacts.filter((c) => (
+                    c.name.toLowerCase().includes(query.toLowerCase())
+                ))
+
         return (
             <div className="list-contacts">
                 <div className="list-contacts-top">
@@ -27,12 +37,12 @@ class ListContacts extends Component{
                         type="text" 
                         className="search-contacts"
                         defaultValue="Search Contacts"
-                        value={this.state.query}
+                        value={query}
                         onChange={(event)=>this.updateQuery(event.target.value)}
                     />
                 </div>
                 <ol className='contact-list'>
-                    {this.props.contacts.map((contact)=>(
+                    {shownContacts.map((contact)=>(
                         <li key={contact.id} className='contact-list-item'>
 
                         <div className='contact-avatar' style={
@@ -45,7 +55,7 @@ class ListContacts extends Component{
                             <p>{contact.name}</p>
                             <p>{contact.handle}</p>
                         </div>
-                        <button className='contact-remove' onClick={()=>this.props.removeContact(contact)}>Remove</button>
+                        <button className='contact-remove' onClick={()=>removeContact(contact)}>Remove</button>
                         </li>
                     ))}
                 </ol>
