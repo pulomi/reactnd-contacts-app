5cb780088fe460841c950d4202117756fecd196b
sniop.safe@gmail.com <4bmnRq9p>
2021-01-22 00:22:48 +0000

    add create contact form

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
-
+import {Link} from 'react-router-dom'
+import ImageInput from './ImageInput'
 class CreateContact extends Component {
 
 
   render() {
     return (
       <div>
-        Create Contact        
+        <Link className='close-create-contact' to='/' />
+        <form className='create-contact-form'>
+        <ImageInput 
+            className='create-contact-avatar-input'
+            name="avatarURL"
+            maxHeight={64}
+        />
+        <div className='create-contact-details'>
+          <input type='text' name='name' placeholder='Name'/>
+          <input type='text' name='handle' placeholder='Handle'/>
+          <button>Add Contact</button>
+        </div>
+        </form>
       </div>
     );
   }
