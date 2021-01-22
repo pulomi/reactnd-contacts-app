e99b96d29867f263b587fcd9d2ba4f9fa845a237
sniop.safe@gmail.com <4bmnRq9p>
2021-01-22 21:26:59 +0000

    use serialize-form to capture form data

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -3,24 +3,34 @@ import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
 import {Link} from 'react-router-dom'
 import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize'
 class CreateContact extends Component {
+
+  onFormSubmit = (event)=>{
+    event.preventDefault();
+    const formValues = serializeForm(event.target, {hash:true});
+    console.log(formValues)
+    if(this.props.onCreateContact){
+      this.props.onCreateContact(formValues)
+    }
+  }
 
 
   render() {
     return (
       <div>
         <Link className='close-create-contact' to='/' />
-        <form className='create-contact-form'>
-        <ImageInput 
-            className='create-contact-avatar-input'
-            name="avatarURL"
-            maxHeight={64}
-        />
-        <div className='create-contact-details'>
-          <input type='text' name='name' placeholder='Name'/>
-          <input type='text' name='handle' placeholder='Handle'/>
-          <button>Add Contact</button>
-        </div>
+        <form className='create-contact-form' onSubmit={this.onFormSubmit}>
+          <ImageInput 
+              className='create-contact-avatar-input'
+              name="avatarURL"
+              maxHeight={64}
+          />
+          <div className='create-contact-details'>
+            <input type='text' name='name' placeholder='Name'/>
+            <input type='text' name='handle' placeholder='Handle'/>
+            <button>Add Contact</button>
+          </div>
         </form>
       </div>
     );
