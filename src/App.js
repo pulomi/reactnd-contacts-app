9e3896d9c141400b9d7ad1a7f8893f60f7d90973
sniop.safe@gmail.com <4bmnRq9p>
2021-01-22 22:07:32 +0000

    compelte create contact

--- src/App.js
+++ src/App.js
@@ -30,6 +30,14 @@ class App extends Component {
 
   }
 
+  createContact = (contact)=>{
+    ContactsAPI.create(contact).then((contact)=>{
+      this.setState((currentState)=>({
+        contacts: currentState.contacts.concat([contact])
+      }))
+      });
+  }
+
   render() {
     return (
       <div>
@@ -38,7 +46,14 @@ class App extends Component {
               contacts={this.state.contacts} 
               removeContact={this.removeContact}/>
         )} />
-        <Route path='/create' component={CreateContact}/>
+        <Route path='/create' render={({history})=>(
+          <CreateContact onCreateContact={
+            (contact) => {
+              this.createContact(contact);
+              history.push("/");
+            }
+          } />
+        )}/>
         
       </div>
     );
